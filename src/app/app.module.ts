ae5ae5e6b12bf0baff89db056c7f751a2f667aac
Lourival Linard <l.linard@gmail.com>
2019-01-22 10:52:36 -0300

    Carregando mais filmes na lista quando a página chega ao final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,16 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {HttpModule} from "@angular/http"
 import { FeedPageModule } from '../pages/feed/feed.module';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { MoovieProvider } from '../providers/moovie/moovie';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ConfiguracoesPageModule } from '../pages/configuracoes/configuracoes.module';
+import { SobrePageModule } from '../pages/sobre/sobre.module';
+import { PerfilPageModule } from '../pages/perfil/perfil.module';
+import { FilmeDetalhesPageModule } from '../pages/filme-detalhes/filme-detalhes.module';
 
 @NgModule({
   declarations: [
@@ -25,7 +33,13 @@ import { IntroPageModule } from '../pages/intro/intro.module';
     BrowserModule,
     IonicModule.forRoot(MyApp),
     FeedPageModule,
-    IntroPageModule
+    IntroPageModule,
+    HttpModule,
+    HttpClientModule,
+    ConfiguracoesPageModule,
+    SobrePageModule,
+    PerfilPageModule,
+    FilmeDetalhesPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -38,7 +52,8 @@ import { IntroPageModule } from '../pages/intro/intro.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MoovieProvider
   ]
 })
 export class AppModule {}
--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,36 +1,116 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { MoovieProvider } from "../../providers/moovie/moovie";
+import { FilmeDetalhesPage } from '../filme-detalhes/filme-detalhes';
 
 /**
  * Generated class for the FeedPage page.
  *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+      MoovieProvider
+  ]
 })
 export class FeedPage {
-
-  public nome_usuario:string = "Ionic Framework";
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public objeto_feed = {
+      titulo:"Charles Franca",
+      data: "November 5, 1955",
+      descricao:"Estou criando um app incrivel...",
+      qntd_likes: 12,
+      qntd_comments: 4,
+      time_comment: "11h ago teste"
   }
 
-  /**
-   * someDoisaumeros
-   */
-  public somaDoisNumeros(num1:number, num2:number): void {
-   // alert("Minha Função funcionou");
-   alert(num1 + num2);
+  public lista_filmes = new Array<any>();
+  public page = 1;
+
+  public nome_usuario:string = "Charles Franca do Codigo";
+  public loader;
+  public refresher;
+  public isRefreshing: boolean = false;
+  public infiniteScroll;
+
+  constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      private movieProvider: MoovieProvider,
+      public loadingCtrl: LoadingController
+      ) {
   }
 
-  ionViewDidLoad() {
-    //console.log('ionViewDidLoad FeedPage');
-    //this.somaDoisNumeros(10, 99);
+  abreCarregando() {
+    this.loader = this.loadingCtrl.create({
+      content: "Carregando filmes..."
+    });
+    this.loader.present();
+  }
+
+  fechaCarregando(){
+    this.loader.dismiss();
+  }
+
+  public somaDoisNumeros(num1:number, num2:number): void{
+      //alert(num1 + num2);
+  }
+
+  doRefresh(refresher) {
+    this.refresher = refresher;
+    this.isRefreshing = true;
+
+    this.carregarFilmes();
+  }
+
+  ionViewDidEnter() {
+    this.carregarFilmes();
+  }
+
+  abrirDetalhes(filme){
+    console.log(filme);
+    this.navCtrl.push(FilmeDetalhesPage, { id: filme.id });
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.infiniteScroll = infiniteScroll;
+    this.carregarFilmes(true);
+  }
+
+  carregarFilmes(newpage: boolean = false){
+    this.abreCarregando();
+    this.movieProvider.getLatestMovies(this.page).subscribe(
+      data=>{
+          const response = (data as any);
+          const objeto_retorno = JSON.parse(response._body);
+
+          if(newpage){
+            this.lista_filmes = this.lista_filmes.concat(objeto_retorno.results);
+            console.log(this.page);
+            console.log(this.lista_filmes);
+            this.infiniteScroll.complete();
+          }else{
+            this.lista_filmes = objeto_retorno.results;
+          }
+
+          this.fechaCarregando();
+          if(this.isRefreshing){
+              this.refresher.complete();
+              this.isRefreshing = false;
+          }
+      }, error => {
+          console.log(error);
+          this.fechaCarregando();
+          if(this.isRefreshing){
+              this.refresher.complete();
+              this.isRefreshing = false;
+          }
+      }
+    )
   }
 
 }
